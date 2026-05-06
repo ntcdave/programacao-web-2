1ed9f1ce479d7f8345ea6c3366487ef3942486bc
Davi Brito <davibrito1282@gmail.com>
2026-05-05 19:55:30 -0500

    feat: initialize Spring Boot project with Maven Wrapper configuration

---
+++ api-banco-digital/src/main/java/br/edu/ifac/api_banco_digital/ApiBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifac.api_banco_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancoDigitalApplication.class, args);
+	}
+
+}
---
+++ api-banco-digital/src/test/java/br/edu/ifac/api_banco_digital/ApiBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifac.api_banco_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
